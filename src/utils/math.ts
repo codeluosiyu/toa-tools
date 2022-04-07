6380184bf067d1240df3d8bd6af0a5ea5dbc6457
codeLuosiyu <2422344142@qq.com>
2022-04-08 05:35:14 +0800

    feat(tools):update math method

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -64,3 +64,65 @@ export const findDuplicates = function (nums: Array<any>) {
 
   return res;
 };
+
+/**
+ * 在M*N网格内筛选出最短路径，寻路
+ * @param grid 0 空、1 障碍
+ * @param k
+ * @returns
+ */
+export const shortestPath = function (grid: Array<Array<number>>, k: number) {
+  const directions = [
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+  ];
+  const height = grid.length;
+  const width = grid[0].length;
+  const dp = Array(height)
+    .fill()
+    .map((_) =>
+      Array(width)
+        .fill()
+        .map((_) => Array(k + 1).fill(Infinity))
+    );
+  dp[0][0][0] = 0;
+  const queue = [[0, 0]];
+  while (queue.length > 0) {
+    const [x, y] = queue.shift();
+    const status = dp[y][x];
+
+    for (const [offsetX, offsetY] of directions) {
+      const nextXY = [x + offsetX, y + offsetY];
+      const [nextX, nextY] = nextXY;
+
+      if (nextX >= 0 && nextY >= 0 && nextX < width && nextY < height) {
+        const nextStatus = dp[nextY][nextX];
+        const isObstacle = grid[nextY][nextX] == 1;
+
+        let available = false;
+        if (!isObstacle) {
+          for (let i = 0; i < k + 1; i++) {
+            const nextStep = status[i] + 1;
+            if (nextStep < nextStatus[i]) {
+              nextStatus[i] = nextStep;
+              available = true;
+            }
+          }
+        } else {
+          for (let i = 0; i < k; i++) {
+            const nextStep = status[i] + 1;
+            if (nextStep < nextStatus[i + 1]) {
+              nextStatus[i + 1] = nextStep;
+              available = true;
+            }
+          }
+        }
+        if (available) queue.push(nextXY);
+      }
+    }
+  }
+  let totalSteps = Math.min(...dp[height - 1][width - 1]);
+  return totalSteps == Infinity ? -1 : totalSteps;
+};
