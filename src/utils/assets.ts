4475017a0a74868434ed1bdd03fabd4c61b00d93
codeLuosiyu <2422344142@qq.com>
2022-04-13 22:33:14 +0800

    feat(img):update assets method

---
+++ src/utils/assets.ts
@@ -0,0 +1,23 @@
+/**
+ * 获取资源后缀
+ * @param obj
+ * @returns
+ */
+export const getAssetsType = async (obj: string) => {
+  // 获取字符串中.之后的所有字符
+  var index = obj.lastIndexOf(".");
+  obj = obj.substring(index + 1, obj.length);
+  return obj;
+};
+
+/**
+ * 已知宽高获取图片相对于屏幕的高度使得自适应
+ * @param obj
+ * @param targetWidth
+ * @returns
+ */
+export const getMediaHeight = async (obj: AnyObject, targetWidth = 750) => {
+  let ratio = obj.width / obj.height;
+  let targetHeight = targetWidth / ratio;
+  return (targetHeight + 2).toFixed(2);
+};
