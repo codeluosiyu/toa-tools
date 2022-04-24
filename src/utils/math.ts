d78574ee28e7e435e81cc716bca4a1b66a151e20
codeLuosiyu <2422344142@qq.com>
2022-04-24 23:26:47 +0800

    feat(math):update math method

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -126,3 +126,177 @@ export const shortestPath = function (grid: Array<Array<number>>, k: number) {
   let totalSteps = Math.min(...dp[height - 1][width - 1]);
   return totalSteps == Infinity ? -1 : totalSteps;
 };
+
+/** 生成指定范围内的随机数 */
+export const randomNum = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+/** 数组乱序 */
+export const arrScrambling = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    const randomIndex = Math.round(Math.random() * (arr.length - 1 - i)) + i;
+    [arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]];
+  }
+  return arr;
+};
+
+/** 数组扁平化 */
+export const flatten = (arr) => {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flatten(arr[i]));
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+
+/** 数组中获取随机数 */
+export const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+/** 生成随机字符串 */
+export const randomString = (len) => {
+  let chars = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz123456789";
+  let strLen = chars.length;
+  let randomStr = "";
+  for (let i = 0; i < len; i++) {
+    randomStr += chars.charAt(Math.floor(Math.random() * strLen));
+  }
+  return randomStr;
+};
+
+/** 字符串首字母大写 */
+export const fistLetterUpper = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+/** 手机号中间四位变成* */
+export const telFormat = (tel) => {
+  tel = String(tel);
+  return tel.substr(0, 3) + "****" + tel.substr(7);
+};
+
+/** 驼峰命名转换成短横线命名 */
+export const getKebabCase = (str) => {
+  return str.replace(/[A-Z]/g, (item) => "-" + item.toLowerCase());
+};
+
+/** 短横线命名转换成驼峰命名 */
+export const getCamelCase = (str) => {
+  return str.replace(/-([a-z])/g, (i, item) => item.toUpperCase());
+};
+
+/** 全角转换为半角 */
+export const toCDB = (str) => {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    var code = str.charCodeAt(i);
+    if (code >= 65281 && code <= 65374) {
+      result += String.fromCharCode(str.charCodeAt(i) - 65248);
+    } else if (code == 12288) {
+      result += String.fromCharCode(str.charCodeAt(i) - 12288 + 32);
+    } else {
+      result += str.charAt(i);
+    }
+  }
+  return result;
+};
+
+/** 半角转换为全角 */
+export const toDBC = (str) => {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    var code = str.charCodeAt(i);
+    if (code >= 33 && code <= 126) {
+      result += String.fromCharCode(str.charCodeAt(i) + 65248);
+    } else if (code == 32) {
+      result += String.fromCharCode(str.charCodeAt(i) + 12288 - 32);
+    } else {
+      result += str.charAt(i);
+    }
+  }
+  return result;
+};
+
+/** 数字转化为大写金额 */
+export const digitUppercase = (n) => {
+  const fraction = ["角", "分"];
+  const digit = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"];
+  const unit = [
+    ["元", "万", "亿"],
+    ["", "拾", "佰", "仟"],
+  ];
+  n = Math.abs(n);
+  let s = "";
+  for (let i = 0; i < fraction.length; i++) {
+    s += (
+      digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]
+    ).replace(/零./, "");
+  }
+  s = s || "整";
+  n = Math.floor(n);
+  for (let i = 0; i < unit[0].length && n > 0; i++) {
+    let p = "";
+    for (let j = 0; j < unit[1].length && n > 0; j++) {
+      p = digit[n % 10] + unit[1][j] + p;
+      n = Math.floor(n / 10);
+    }
+    s = p.replace(/(零.)*零$/, "").replace(/^$/, "零") + unit[0][i] + s;
+  }
+  return s
+    .replace(/(零.)*零元/, "元")
+    .replace(/(零.)+/g, "零")
+    .replace(/^整$/, "零元整");
+};
+
+/** 数字转化为中文数字 */
+export const intToChinese = (value) => {
+  const str = String(value);
+  const len = str.length - 1;
+  const idxs = [
+    "",
+    "十",
+    "百",
+    "千",
+    "万",
+    "十",
+    "百",
+    "千",
+    "亿",
+    "十",
+    "百",
+    "千",
+    "万",
+    "十",
+    "百",
+    "千",
+    "亿",
+  ];
+  const num = ["零", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+  return str.replace(/([1-9]|0+)/g, ($, $1, idx, full) => {
+    let pos = 0;
+    if ($1[0] !== "0") {
+      pos = len - idx;
+      if (idx == 0 && $1[0] == 1 && idxs[len - idx] == "十") {
+        return idxs[len - idx];
+      }
+      return num[$1[0]] + idxs[len - idx];
+    } else {
+      let left = len - idx;
+      let right = len - idx + $1.length;
+      if (Math.floor(right / 4) - Math.floor(left / 4) > 0) {
+        pos = left - (left % 4);
+      }
+      if (pos) {
+        return idxs[pos] + num[$1[0]];
+      } else if (idx + $1.length >= len) {
+        return "";
+      } else {
+        return num[$1[0]];
+      }
+    }
+  });
+};
