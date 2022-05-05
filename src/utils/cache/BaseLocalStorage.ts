51b388c238e2dfd6f37b3a1a98e244556122db2f
codeLuosiyu <2422344142@qq.com>
2022-05-05 23:32:23 +0800

    feat(cache):update localstorage method in client

--- src/utils/cache/BaseLocalStorage.ts
+++ src/utils/cache/BaseLocalStorage.ts
@@ -1,6 +1,6 @@
+/** 兼容浏览器和小程序的缓存方法 */
 export abstract class BaseLocalStorage {
   keyPrefix = "";
-
   abstract getItem<T = unknown>(key: string): T | undefined;
   abstract setItem(key: string, value: any): void;
   abstract removeItem(key: string): void;
