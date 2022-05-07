fb29eb07b1f10816ed599c03431720a6a98c3639
codeLuosiyu <2422344142@qq.com>
2022-05-07 21:15:05 +0800

    feat(cache):update cache methods

--- src/utils/cache/BaseCache.ts
+++ src/utils/cache/BaseCache.ts
@@ -51,3 +51,18 @@ export const localClear = () => {
 export const removeSession = (name) => {
   return sessionStorage.removeItem(name);
 };
+
+export const getCookit = (key) => {
+  if (document.cookie.length > 0) {
+    let c_start = document.cookie.indexOf(key + "=");
+    if (c_start != -1) {
+      c_start = c_start + key.length + 1;
+      let c_end = document.cookie.indexOf(";", c_start);
+      if (c_end == -1) {
+        c_end = document.cookie.length;
+      }
+      return unescape(document.cookie.substring(c_start, c_end));
+    }
+  }
+  return "";
+};
