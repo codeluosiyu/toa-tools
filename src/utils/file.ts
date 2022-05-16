91292d0e7fea801981130ed31e24c31e1dbc5d2b
codeLuosiyu <2422344142@qq.com>
2022-05-16 20:24:19 +0800

    feat(file):update file method about size

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -15,3 +15,24 @@ export const getFolderNames = (names: string[]): string[] => {
     return s;
   });
 };
+
+/**
+ * 计算文件占用体积
+ * @param size
+ * @returns
+ */
+export const handleSize = (size) => {
+  const KB = size / 1024;
+  const MB = KB / 1024;
+  const GB = MB / 1024;
+  const TB = GB / 1024;
+  if (MB < 1) {
+    return `${KB.toFixed(2)} KB`;
+  } else if (GB < 1) {
+    return `${MB.toFixed(2)} MB`;
+  } else if (TB < 1) {
+    return `${GB.toFixed(2)} GB`;
+  } else {
+    return `${TB.toFixed(2)} TB`;
+  }
+};
