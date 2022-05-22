432b5c8d24015a8ebc3ed6f34ffd7261f4c6723e
codeLuosiyu <2422344142@qq.com>
2022-05-22 21:33:06 +0800

    feat(tools):update string method

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -71,3 +71,14 @@ export const getExtension = (str) => {
   str = str.split(".");
   return str.pop();
 };
+
+/**
+ * 字符串大小写取反
+ * @param str
+ * @returns
+ */
+export const transfer = (str) => {
+  return str.replace(/[a-zA-Z]/g, (match) => {
+    return /[a-z]/.test(match) ? match.toUpperCase() : match.toLowerCase();
+  });
+};
