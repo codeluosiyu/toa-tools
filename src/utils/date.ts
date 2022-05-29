788fbbe8e61838ba1d0094d66663a0a21de188e4
codeLuosiyu <2422344142@qq.com>
2022-05-29 19:01:51 +0800

    feat(date):update date method

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -220,14 +220,14 @@ export const timerToUTC = (timeStamp, returnType) => {
     date.setTime(timeStamp);
   }
   var y = date.getFullYear();
-  var m = date.getMonth() + 1;
+  var m: String | Number = date.getMonth() + 1;
   m = m < 10 ? "0" + m : m;
-  var d = date.getDate();
+  var d: String | Number = date.getDate();
   d = d < 10 ? "0" + d : d;
-  var h = date.getHours();
+  var h: String | Number = date.getHours();
   h = h < 10 ? "0" + h : h;
-  var minute = date.getMinutes();
-  var second = date.getSeconds();
+  var minute: String | Number = date.getMinutes();
+  var second: String | Number = date.getSeconds();
   minute = minute < 10 ? "0" + minute : minute;
   second = second < 10 ? "0" + second : second;
   if (returnType == "str") {
