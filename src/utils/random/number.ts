1eefd38a5177ef71bf34c953c0fa2c7a8ee65ed1
codeLuosiyu <2422344142@qq.com>
2022-06-13 17:22:44 +0800

    feat(array):update arrat method about random

--- src/utils/random/number.ts
+++ src/utils/random/number.ts
@@ -13,3 +13,18 @@ export const randomString = function (len) {
   }
   return pwd;
 };
+
+/**
+ * 数组乱序
+ * @param arr 
+ * @returns 
+ */
+export const shuffleArray = (arr) => {
+  let m = arr.length;
+  while (m > 1) {
+    let index = Math.floor(Math.random() * m--);
+    [arr[m], arr[index]] = [arr[index], arr[m]]
+  }
+  return arr;
+}
+
