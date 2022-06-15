4a88a32b9a5a642ec402f7540941da39661bc604
codeLuosiyu <2422344142@qq.com>
2022-06-15 22:43:12 +0800

    feat(base):update base method about trim

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -267,5 +267,28 @@ export const compareVersion = function (version1, version2) {
   return result;
 };
 
+/**
+ * 手写trim方法
+ * @param str 
+ */
+export const trim = (str) => {
+  var whitespace = ' \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\n\
+  \u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000';
+  for (var i = 0; i < str.length; i++) {
+    if (whitespace.indexOf(str.charAt(i)) === -1) {
+      str = str.substring(i)
+      break;
+    }
+  }
+
+  for (i = str.length - 1; i >= 0; i--) {
+    if (whitespace.indexOf(str.charAt(i)) === -1) {
+      str = str.substring(0, i + 1)
+      break;
+    }
+  }
+  return whitespace.indexOf(str.charAt(0)) === -1 ? str : ''
+}
+
 
 
