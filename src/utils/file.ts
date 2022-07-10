21f83f63f84c024ada796a92835a65d6bf7242e7
codeLuosiyu <2422344142@qq.com>
2022-07-10 23:53:12 +0800

    feat(file):update file method

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -36,3 +36,48 @@ export const handleSize = (size) => {
     return `${TB.toFixed(2)} TB`;
   }
 };
+
+
+function charToBinary(text) {
+  var code = "";
+  for (let i of text) {
+    // 字符编码
+    let number = i.charCodeAt().toString(2);
+    // 1 bytes = 8bit，将 number 不足8位的0补上
+    for (let a = 0; a <= 8 - number.length; a++) {
+      number = 0 + number;
+    }
+    code += number;
+  }
+  return code;
+}
+
+/**
+ * 将二进制数据每 6bit 位替换成一个 base64 字符
+ * @param code 
+ * @returns 
+ */
+export const binaryTobase64 = (code) => {
+  let base64Code = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  let res = '';
+  // 1 bytes = 8bit，6bit 位替换成一个 base64 字符
+  // 所以每 3 bytes 的数据，能成功替换成 4 个 base64 字符
+
+  // 对不足 24 bit (也就是 3 bytes) 的情况进行特殊处理
+  if (code.length % 24 === 8) {
+    code += '0000';
+    res += '=='
+  }
+  if (code.length % 24 === 16) {
+    code += '00';
+    res += '='
+  }
+
+  let encode = '';
+  // code 按 6bit 一组，转换为
+  for (let i = 0; i < code.length; i += 6) {
+    let item = code.slice(i, i + 6);
+    encode += base64Code[parseInt(item, 2)];
+  }
+  return encode + res;
+}
